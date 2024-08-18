45fe4c2166616be7ce0339b72787102f07da7f8c
0xlunarcodes <angel817jr@gmail.com>
2024-08-18 01:16:06 -0500

    added a new class of text and images, for easier creation of surfaces and collision detection of surfaces made easier as well.

--- graphics.py
+++ graphics.py
@@ -1,24 +1,67 @@
 import pygame
 #fonts
 pygame.init()
-textColor = (48, 74, 57)
 
+#text specific variables
+textColor = (48, 74, 57)
 font_L = pygame.font.Font("gamefiles/Daydream.ttf", 30)
 font_M = pygame.font.Font("gamefiles/Daydream.ttf", 20)
 font_S = pygame.font.Font("gamefiles/Daydream.ttf", 10)
+textDict = {} #Surface Dictionary
 
-font_L.render('study dog !', True, textColor)
+#image specific variables
+imageDict = {}
 
-surfaces = {} #Surface Dictionary
+#pygame.Rect((80, 275), start_surf.get_size()).collidepoint(mouse) and mouseclick
 
-def genText(name, text, size) :
-    if size == "s" :
-        font = font_S
-    elif size == "m" :
-        font = font_M
-    else :
-        font = font_L
+class ImageObj :
+    def __init__(self, surface, position=None) :
+        self.surf = surface
+        self.pos = position
     
+    def get(self) :
+        print(self.surf, self.pos)
+        return(self.surf, self.pos)
+    
+    def isClicked(self, mousePosition, mouseClick) -> bool :
+        if (pygame.Rect(self.pos, self.surf.get_size())).collidepoint(mousePosition) and mouseClick :
+            return True
+        else :
+            return False
+class TextObj:    
+    def __init__(self, surface, position=None):
+        self.surf = surface
+        self.pos = position
+        print("Text Obj Init")
+        
+    def get(self) :
+        return (self.surf, self.pos)
+    
+    def getColor(self) :
+        return self.surf.textColor
+    
+def genText(name, text, position=None, size=None) :         
+    if size == "S" :
+        font = font_S
+    elif size == "L" :
+        font = font_L
+    else :
+        font = font_M
+        
     new_surf = font.render(text, True, textColor)
-    surfaces[name + "Surf"] = new_surf
-    print(name + "Surf Created")
+    textDict[name] = TextObj(new_surf, position)
+    
+def genImage(name, image, position=None, transparency=None, scale=None) :
+    if transparency == True :
+        newSurf = pygame.image.load_extended(image).convert_alpha()
+        print("timg init")
+    else :
+        newSurf = pygame.image.load(image).convert()
+        print("img init")
+    if scale != None :
+        newSurf = pygame.transform.scale_by(newSurf, scale)
+        imageDict[name] = ImageObj(newSurf,position)
+    else :
+        imageDict[name] = ImageObj(newSurf,position)   
+        
+#if image pos,  image surf size.collidepoint mouse, and mouse click
--- main.py
+++ main.py
@@ -9,19 +9,9 @@ clock = pygame.time.Clock()
 running = True
 dt = 0 #deltatime
 
-#fonts
-font_L = pygame.font.Font("gamefiles/Daydream.ttf", 30)
-font_M = pygame.font.Font("gamefiles/Daydream.ttf", 20)
-font_S = pygame.font.Font("gamefiles/Daydream.ttf", 10)
-
-#colors
-red = (255, 0, 0)
-black = (0, 0, 0)
-white = (255, 255, 255)
 bgColor = (137, 199, 156)
-textColor = (48, 74, 57)
 
-#savadata
+#savadata needs its own module
 #creates savedata if not exist, writes default values
 savefile = open("gamefiles/savedata.txt", "r")
 savedata= savefile.readlines()
@@ -35,15 +25,13 @@ mouseclick = False
 mouse = pygame.mouse.get_pos()
 pygame.mixer.music.load("gamefiles/start.ogg")
 
-#positions
-dog_pos = pygame.Vector2(50,55)
-
 #img/surfaces
-graphics.genText('title', 'study dog !', 'l')
+graphics.genText('Title', 'study dog !', (80, 10))
+graphics.genImage('Title', 'dogpics/titledog.png', pygame.Vector2(50,55), True)
+graphics.genImage('Start Button', 'gamefiles/start.png', (80, 275), True, 4)
 gf_surf = pygame.image.load("gamefiles/gf.jpg").convert()
-titledog_surf = (pygame.image.load("dogpics/titledog.png").convert_alpha())
-start_img = pygame.image.load_extended("gamefiles/start.png").convert_alpha()
-start_surf = pygame.transform.scale_by(start_img, 4)
+#start_img = pygame.image.load_extended("gamefiles/start.png").convert_alpha()
+#start_surf = pygame.transform.scale_by(start_img, 4)
 
 
 while running:    
@@ -56,8 +44,7 @@ while running:
             
         if event.type == pygame.MOUSEBUTTONDOWN: 
             mouseclick = True
-            
-            
+    
         if event.type == pygame.MOUSEBUTTONUP:
             mouseclick = False
 
@@ -67,29 +54,31 @@ while running:
     # all previous code in this while loop deals with events, framerate and clearing the previous frame. 
     #other code , actual code for the game in the while loop will go below this line
     
-    status_surf = font_S.render(f"<3 {savedata[0].rstrip(savedata[0][-1])}!  hunger {savedata[1].rstrip(savedata[1][-1])}!  intel {savedata[2].rstrip(savedata[2][-1])}!", True, textColor)
-
+    #status_surf = font_S.render(f"<3 {savedata[0].rstrip(savedata[0][-1])}!  hunger {savedata[1].rstrip(savedata[1][-1])}!  intel {savedata[2].rstrip(savedata[2][-1])}!", True, textColor)
+    graphics.genText('status', f"<3 {savedata[0].rstrip(savedata[0][-1])}!  hunger {savedata[1].rstrip(savedata[1][-1])}!  intel {savedata[2].rstrip(savedata[2][-1])}!", (40, 10), "S")
 
     match(gameScreen):
         case "title":
             #print("title screen")
-            screen.blit(graphics.surfaces['titleSurf'], (40, 10)) #Boss, we figured it out, time to build outer haven
-            screen.blit(titledog_surf, (dog_pos))
-                 
+            screen.blit(*graphics.textDict['Title'].get()) #Boss, we figured it out, time to build outer haven
+            screen.blit(*graphics.imageDict['Title'].get())
             
             #start button
             #start_surf is the button surface object, need to convert to rect object to get collision with mouse with colliedpoint
-            screen.blit(start_surf, (80,275))
-            if pygame.Rect((80, 275), start_surf.get_size()).collidepoint(mouse) and mouseclick: 
+            screen.blit(*graphics.imageDict['Start Button'].get())
+            if(graphics.imageDict['Start Button'].isClicked(mouse, mouseclick)) :
                 print("button active")
                 pygame.mixer.music.play()
                 gameScreen = "main"
+            #if pygame.Rect((80, 275), start_surf.get_size()).collidepoint(mouse) and mouseclick: 
+            #    print("button active")
+            #    pygame.mixer.music.play()
+            #    gameScreen = "main"
                 
         case "main" : 
-            #print(savedata[1])
-            screen.blit(status_surf, (40, 10))
-            screen.blit(titledog_surf, (dog_pos))
-                  
+            print(savedata[1])
+            screen.blit(*graphics.textDict['status'].get())
+            #screen.blit(titledog_surf, (dog_pos))
 
         case _:
             print("no current gamescreen!!")
